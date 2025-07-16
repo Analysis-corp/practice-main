b58eae219cc92e0b5a1b92d362dccb1d4eae074c
Analysis <132825494+Analysis-corp@users.noreply.github.com>
2025-07-16 16:40:20 +0330

    add features

--- P10/script.js
+++ P10/script.js
@@ -5,18 +5,42 @@ const taskContainer = document.querySelector("#task-container");
 const tasks = [];
 
 createBTN.addEventListener("click", () => {
-  tasks.push(inputTittle.value);
+  // tasks.push(inputTittle.value);
+  tasks.push({
+    tittle: inputTittle.value,
+  });
   console.log("new task created :", tasks);
   inputTittle.value = "";
+
+  renderTasks();
 });
 
 function renderTasks() {
   taskContainer.innerHTML = "";
-  const newTask = document.createElement("div");
-  newTask.classList.add("task");
+  for (let counter = 0; counter < tasks.length; counter++) {
+    const newTask = document.createElement("div");
+    newTask.classList.add("task");
 
-  const numberContainer = document.createElement("div");
-  numberContainer.classList.add("number-container");
-  const taskNumber = document.createElement("p");
-  taskNumber.classList.add("task-number");
+    // create number  container , add task number
+    const numberContainer = document.createElement("div");
+    numberContainer.classList.add("number-container");
+    const taskNumber = document.createElement("p");
+    taskNumber.classList.add("task-number");
+    taskNumber.innerText = counter;
+
+    numberContainer.appendChild(taskNumber);
+    taskContainer.appendChild(numberContainer);
+
+    // console.log(taskContainer);
+
+    //create tittle
+    const newTittle = document.createElement("p");
+    newTittle.innerText = tasks[counter].tittle;
+    newTask.appendChild(newTittle);
+    console.log(newTask);
+    taskContainer.appendChild(newTask);
+
+    //add img
+    const imageContainer = document.createElement("div");
+  }
 }
