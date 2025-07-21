433ee5a61c46bda610b1d22ca07d8cc56ad1a92f
Analysis <132825494+Analysis-corp@users.noreply.github.com>
2025-07-21 23:39:37 +0330

    UP TO DATE

--- Cipher/script.js
+++ Cipher/script.js
@@ -35,26 +35,47 @@ const offset = 1;
 let corrAlphabet = [];
 
 for (let i = 0; i < alphabet.length; i++) {
-  if (i + offset < alphabet.length) {
-    corrAlphabet[i + offset] = alphabet[i];
-  } else {
+  if (i + offset < alphabet.length) corrAlphabet[i + offset] = alphabet[i];
+  else {
     corrAlphabet[i + offset - alphabet.length] = alphabet[i];
   }
 }
-
 console.log(corrAlphabet);
 
 function cipher() {
-  const text = inputText.value.toUpperCase();
-  console.log(text);
+  const text = prompt("Enter your text").toUpperCase();
+  if (!text) {
+    const confirmInvalid = confirm("Enter a valid text");
+    if (confirmInvalid) return cipher();
+    return false;
+  }
+  const textArray = text.split("");
 
   let result = "";
-  const textArray = text.split("");
-  console.log(textArray);
-
   for (let i = 0; i < textArray.length; i++) {
     const alphabetIndex = alphabet.indexOf(textArray[i]);
     result += corrAlphabet[alphabetIndex];
   }
-  console.log(result);
+  alert("your cipher is :  " + result);
+}
+
+const decipherText = document.getElementById("decipher-text");
+const decipherBTN = document.getElementById("decipher-button");
+const decipherRES = document.getElementById("decipher-result");
+
+function decipher() {
+  const text = prompt("Enter your cipher text");
+  if (!text) {
+    const confirmInvalid = confirm("Enter a valid text");
+    if (confirmInvalid) return decipher();
+    return false;
+  }
+  const textArray = text.split("");
+
+  let result = "";
+  for (let i = 0; i < textArray.length; i++) {
+    const corrAlphabetIndex = corrAlphabet.indexOf(textArray[i]);
+    result += alphabet[corrAlphabetIndex];
+  }
+  alert("your decipher is :   " + result);
 }
